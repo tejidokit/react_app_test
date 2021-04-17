8d8e33472ae63e01f4a65c6df2ad305ca19b9101
Kit Tejido <kittejido@gmail.com>
2021-04-17 13:23:01 -0600

    react automatic refresh

--- app/MAIN.js
+++ app/MAIN.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom'
 function ExampleComponent() {
     return (
         <div>
-            <h1>This is a test</h1>
+            <h1>This is a test!</h1>
             <p>The sky is blue</p>
         </div>
     )
 }
 
 ReactDOM.render(<ExampleComponent/>, document.querySelector("#app"));
+
+// react automatic refresh
+if (module.hot) {
+    module.hot.accept()
+}
