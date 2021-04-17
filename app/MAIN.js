8b28f3e22c0ecb29ac6628c4d2a5a8d1e2b8f3f4
Kit Tejido <kittejido@gmail.com>
2021-04-17 13:06:54 -0600

    react set up

---
+++ app/MAIN.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function ExampleComponent() {
+    return (
+        <div>
+            <h1>This is a test</h1>
+            <p>The sky is blue</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<ExampleComponent/>, document.querySelector("#app"));
